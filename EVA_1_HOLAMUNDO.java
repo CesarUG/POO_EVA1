1b1b7b3243648cc070a0a4d613701ca6b3a46a84
CesarUG <105247039+CesarUG@users.noreply.github.com>
2023-02-07 09:43:24 -0600

    Add files via upload

---
+++ EVA_1_HOLAMUNDO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_1_holamundo;
+
+/**
+ *
+ * @author Cesar
+ */
+public class EVA_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prueba");
+    }
+}
