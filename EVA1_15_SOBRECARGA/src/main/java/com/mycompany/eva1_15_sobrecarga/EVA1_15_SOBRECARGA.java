f665387f5000cb26912d4586ef61558c458b2e6a
CesarUG <105247039+CesarUG@users.noreply.github.com>
2023-02-23 09:50:36 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/main/java/com/mycompany/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.eva1_15_sobrecarga;
+
+/**
+ *
+ * @author Cesar
+ */
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        /*Diferentes println: Son 10
+        Métodos, para imprimir diferenes tipos de datos*/
+        /*Para crear sobrecargas de métodos la firma del metodo debe ser unica*/
+        System.out.println("Hello World!");
+        System.out.println(100);
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+       
+    }
+}
